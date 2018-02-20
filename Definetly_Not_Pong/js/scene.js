e378b7227e101f7c6fb2dfd66c7681f89cd12bd8
Unknown <a.canutperdigon@gmail.com>
2018-02-20 17:54:10 +0100

    Escena base.

---
+++ Definetly_Not_Pong/js/scene.js
@@ -0,0 +1,15 @@
+var definetlyNotPong = definetlyNotPong || {};
+
+definetlyNotPong.scene = {
+    preload:function(){
+        this.game.stage.backgroundColor = "#ff0000";
+    },
+    
+    create:function(){
+        
+    },
+    
+    update:function(){
+        
+    }
+}

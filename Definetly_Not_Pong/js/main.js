4134ad6e5294e25ea7b6eeef02b28bd1ca006137
Devildrake <divadarrei@gmail.com>
2018-02-24 19:17:30 +0100

    Establecidos los disparois (Verdes y rojos), tanto el prefab como los grupos, como la generación por Input de usuario Establecidos los grupos de barreras

--- Definetly_Not_Pong/js/main.js
+++ Definetly_Not_Pong/js/main.js
@@ -1,10 +1,10 @@
-var definetlyNotPong = definetlyNotPong || {};
+var definatelyNotPong = definatelyNotPong || {};
 
 var GameOptions = {
     gameWidth:800,
     gameHeight:540
 };
 
-definetlyNotPong.game = new Phaser.Game(GameOptions.gameWidth,GameOptions.gameHeight,Phaser.AUTO, null, this, false, false);
-definetlyNotPong.game.state.add("scene", definetlyNotPong.scene);
-definetlyNotPong.game.state.start("scene");
+definatelyNotPong.game = new Phaser.Game(GameOptions.gameWidth,GameOptions.gameHeight,Phaser.AUTO, null, this, false, false);
+definatelyNotPong.game.state.add("scene", definatelyNotPong.scene);
+definatelyNotPong.game.state.start("scene");
--- Definetly_Not_Pong/js/scene.js
+++ Definetly_Not_Pong/js/scene.js
@@ -1,15 +1,89 @@
-var definetlyNotPong = definetlyNotPong || {};
+var definatelyNotPong = definatelyNotPong || {};
 
-definetlyNotPong.scene = {
+definatelyNotPong.scene = {
     preload:function(){
-        this.game.stage.backgroundColor = "#ff0000";
+        this.game.stage.backgroundColor = "#ffffff";
+        this.load.image("RedLaser","img/RedLaser.png");
+        this.load.image("GreenLaser","img/GreenLaser.png");
+        this.loadProjectiles();
+        
+    },
+    
+    inputs:function(){
+        if(this.greenShotKey.isDown&&this.greenShotKey.downDuration(1)){
+            //INSERTAR POSICIÓN PERSONAJES
+            this.createGreenProjectile(80,20);
+        }
+        
+        if(this.redShotKey.isDown&&this.redShotKey.downDuration(1)){
+            this.createRedProjectile(440,20);
+        }
     },
     
     create:function(){
+        //this.oktorok = new definatelyNotPong.ProjectilePrefab(this.game,80,20);
+        //this.game.add.existing(this.oktorok);
+
+        
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+	this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);        
+    this.redShotKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    this.greenShotKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+    this.redPowerUpKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    this.greenPowerUpKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
         
     },
     
     update:function(){
+        this.inputs();
+    },
+    
+    
+    loadProjectiles:function(){
+    
+        console.log("Bruh");
+        this.redProjectiles = this.add.group();
+        this.redProjectiles.enableBody = true;
+
+        this.greenProjectiles = this.add.group();
+        this.greenProjectiles.enableBody = true;
+
+    },
+    
+    loadBarriers:function(){
+    
+        this.greenBarriers = this.add.group();
+        this.greenBarriers.enableBody = true;
         
+        this.redBarriers = this.add.group();
+        this.redBarriers.enableBody = true;
+
+    },
+        
+    createRedProjectile:function(posX,posY){
+        var projectile = this.redProjectiles.getFirstExists(false);
+        if(!projectile){
+            projectile = new definatelyNotPong.ProjectilePrefab(this.game,posX,posY);
+            this.redProjectiles.add(projectile);
+        }else{
+            particles.Alive = true;
+            particles.reset(posX+8,posY+8);
+        }
+    },
+        
+    createGreenProjectile:function(posX,posY){
+        var projectile = this.greenProjectiles.getFirstExists(false);
+        if(!projectile){
+            projectile = new definatelyNotPong.ProjectilePrefab(this.game,posX,posY);
+            this.greenProjectiles.add(projectile);
+        }else{
+            particles.Alive = true;
+            particles.reset(posX+8,posY+8);
+        }
     }
+    
+    
+    
+    
+    
 }
